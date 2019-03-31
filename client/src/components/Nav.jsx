2fda394c8a011a621e8075b85031c06b6b5c0f06
shirin <shirinanlen@gmail.com>
2019-03-30 21:17:02 -0400

    started a nav

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Nav(){
+  return (
+    <div className='header'>
+    <img src={"https://i.imgur.com/5tV6auM.png"} alt='logo' className='logo'/>
+    <a className='logout' href=''>To Do List</a>
+    <a className='logout' href=''>Logout</a>
+    </div>
+  )
+}
+  export default Nav
